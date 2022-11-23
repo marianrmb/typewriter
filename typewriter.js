9d2f6ff7a39a6e59f48eab97318b3339207eef8e
Marian <mrmoussa@ualberta.ca>
2022-11-23 07:06:54 +0000

    animated sentence

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hey I actually own a typewriter";
+let delay = 0
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);// print the char here
+  }, delay += 50) // <= 1s delay to make it noticeable. Can dial it down later.
+}
+
+setTimeout(() => {
+  console.log(" ")}, delay);
